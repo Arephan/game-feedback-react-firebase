89215f307185fabd98bf73e265876c0a425f3a6a
han kim <hankim@hans-MacBook-Air.local>
2019-12-07 13:56:54 -0500

    Add comment & stars input for dashboard

--- src/components/protected/Dashboard.js
+++ src/components/protected/Dashboard.js
@@ -1,11 +1,57 @@
 import React, { Component } from 'react'
+import Stars from 'simple-rating-stars';
 
 export default class Dashboard extends Component {
-  render () {
+  constructor(props) {
+    super(props);
+    this.state = { comment: "", stars: 0 };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert('A comment was submitted: ' + this.state.comment + ' Stars: ' + this.state.stars);
+    event.preventDefault();
+  }
+
+  render() {
     return (
-      <div>
-        Dashboard. This is a protected route. You can only see this if you're authed.
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Comment:
+          <input type="text"
+            name="comment"
+            value={this.state.comment}
+            onChange={this.handleChange}
+            required />
+        </label>
+        <br />
+        <label>
+          Stars:
+          <input type="number"
+            name="stars"
+            value={this.state.stars}
+            onChange={this.handleChange}
+            min="1"
+            max="5"
+            required />
+        </label>
+        <br />
+        <label>
+          Game:
+          <select name="Icecream Flavours">
+            <option value="double chocolate">Double Chocolate</option>
+            <option value="vanilla">Vanilla</option>
+            <option value="strawberry">Strawberry</option>
+            <option value="caramel">Caramel</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     )
   }
 }
