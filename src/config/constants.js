9fc15ab98ce24c8fc113be1b588a2b7ce882d552
han kim <hankim@hans-MacBook-Air.local>
2019-12-07 11:36:13 -0500

    Edit firebase config

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,9 +1,9 @@
 import firebase from 'firebase'
 
 const config = {
-  apiKey: "AIzaSyDHL6JFTyBcaV60WpE4yXfeO0aZbzA9Xbk",
-  authDomain: "practice-auth.firebaseapp.com",
-  databaseURL: "https://practice-auth.firebaseio.com",
+  apiKey: "AIzaSyCt6M2EhYF_VrFwVsthF0F52R3_Q6w-LNg",
+  authDomain: "game-feedback-40417.firebaseapp.com",
+  databaseURL: "https://game-feedback-40417.firebaseio.com"
 }
 
 firebase.initializeApp(config)
